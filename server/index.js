df441ebc7c8427d25638ff2b068bcb54f986581d
Ayse Akdag <ayseakdag012@gmail.com>
2022-09-16 17:00:26 +0300

    new routes added

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import { join, dirname } from "path";
-import { Low, JSONFile } from "lowdb";
-import { fileURLToPath } from "url";
+import express from 'express';
+import { join, dirname } from 'path';
+import { Low, JSONFile } from 'lowdb';
+import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Use JSON file for storage
-const file = join(__dirname, "db.json");
+const file = join(__dirname, '../db.json');
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
@@ -18,28 +18,27 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.post("/groupAdd", async (req, res) => {
-  console.log("🚀 ~ file: index.js ~ line 8 ~ app.get ~ req", req.body);
+app.post('/groupAdd', async (req, res) => {
   await db.read();
-
-  //   db.data ||= { posts: [] };
-  db.data = db.data || { posts: [] }; // for node < v15.x
-
-  // Create and query items using plain JS
-  db.data.posts.push(req.body);
-  console.log("🚀 ~ file: index.js ~ line 30 ~ app.post ~ db", db);
-
+  db.data.groups = db.data.groups || []; // for node < v15.x
+  db.data.groups.push(req.body);
   await db.write();
-
-  res.json({ message: "Grup Eklendi!" });
+  res.json({ message: 'Grup Eklendi!' });
 });
-app.post("/getGroup", async (req, res) => {
+app.post('/propertiesAdd', async (req, res) => {
   await db.read();
-  res.json({ data: db.data.posts });
+  db.data.properties = db.data.properties || []; // for node < v15.x
+  db.data.properties.push(req.body);
+  await db.write();
+  res.json({ message: 'Özellik Eklendi!' });
 });
-app.post("/deviceAdd", (req, res) => {
-  console.log("🚀 ~ file: index.js ~ line 8 ~ app.get ~ req", req.body);
-  res.json({ message: "Cihaz eklendi!" });
+app.post('/getGroup', async (req, res) => {
+  await db.read();
+  res.json({ data: db.data.groups });
+});
+app.post('/deviceAdd', (req, res) => {
+  console.log('🚀 ~ file: index.js ~ line 8 ~ app.get ~ req', req.body);
+  res.json({ message: 'Cihaz eklendi!' });
 });
 
 app.listen(PORT, () => {
